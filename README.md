8b2afe9ae51d37958f8b6783ff8e49fa670e7eb3
L <1053801783@qq.com>
2019-12-19 17:09:29 +0800

    publish 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # l-mini-cli
 用于开发小程序的脚手架工具
+
+# 安装项目
+
+> yarn global add lmini
+
+# 创建项目
+
+> lmini init project-mini
+
+使用这条命令会下载 [小程序模板](https://github.com/AboyL/l-mini) 项目
+里面内置了
+1. gulp脚手架
+2. less转化
+3. px转化rpx
+4. eslint
+5. git hook
+6. 监控与错误处理
+7. http处理
+
+...
+
+具体内容可以参考项目该项目的文档
+
+# 创建页面
+> lmini g page pageDir/pageName
+
+# 创建组件
+> lmini g component componentDir/componentName
+
+或者
+
+> lmini g comp componentDir/componentName
---
+++ release.js
@@ -0,0 +1,51 @@
+const program = require("commander");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs')
+
+program.parse(process.argv);
+
+const packageJson = require("./package.json")
+const version = packageJson.version
+
+
+console.log('version', version);
+let newVersion = program.args[0]
+const checkVersion = (version) => {
+  const arr = version.split('.')
+  console.log(typeof arr[0]);
+  if (arr.length !== 3) {
+    return false
+  }
+  if (arr.map(v => parseInt(v)).some(v => typeof v !== 'number')) {
+    return false
+  }
+  return true
+}
+const getNewVersion = (old) => {
+  const arr = old.split('.')
+  if (!checkVersion(old)) {
+    throw '请输入正确的版本号'
+  }
+  return [arr[0], arr[1], arr[2] - 0 + 1].join('.')
+}
+
+if (!newVersion) {
+  newVersion = getNewVersion(version)
+} else {
+  if (!checkVersion(newVersion)) {
+    throw '请输入正确的版本号'
+  }
+}
+
+packageJson.version = newVersion
+fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, '\t'))
+// 执行git 以及npm命令
+async function publishVersion (version) {
+  await exec('git add .')
+  await exec(`git commit -m "publish ${version}"`)
+  await exec(`git tag -a ${version} -m "publish ${version}"`)
+  await exec(`git push origin --tags`)
+  await exec(`npm publish`)
+}
+publishVersion(newVersion)
